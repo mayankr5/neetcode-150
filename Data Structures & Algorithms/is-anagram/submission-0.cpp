899e048f131c03923b195248baceed062bfbe902
Mayank Rathor <90903452+mayankr5@users.noreply.github.com>
2026-05-19 00:21:33 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(26, 0);
+        for(char c: s)
+            freq[c-'a']++;
+        
+        for(char c: t)
+            freq[c-'a']--;
+
+        for(int i: freq)
+            if(i != 0)
+                return false;
+
+        return true;
+    }
+};
