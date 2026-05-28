f4fb1cce010ce9dd38231bcce0b16b1994db4786
Mayank Rathor <90903452+mayankr5@users.noreply.github.com>
2026-05-29 01:15:15 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<vector<int>, vector<string>> mp;
+        for(string s: strs){
+            vector<int> f(26, 0);
+            for(int c: s){
+                f[c-'a']++;
+            }
+            mp[f].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto &it: mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
