70185f6bbddd8aa48d892bd219aff7b38f5fb783
Mayank Rathor <90903452+mayankr5@users.noreply.github.com>
2026-09-10 14:09:34 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> stk;
+        int sz = temperatures.size();
+        vector<int> res;
+        for(int i = sz-1; i >= 0; i--){
+            if(stk.empty()){
+                res.push_back(0);
+            }else{
+                while(!stk.empty() && stk.top().first <= temperatures[i]){
+                    stk.pop();
+                }
+                if(stk.empty()){
+                    res.push_back(0);
+                }else{
+                    res.push_back(stk.top().second - i);
+                }
+            }
+            stk.push({temperatures[i], i});
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
