d3330202b341e9460dd7593bbac752ff5f4b9a24
Mayank Rathor <90903452+mayankr5@users.noreply.github.com>
2026-09-16 17:29:51 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool willEat(int k, vector<int> &piles, int h){
+        int timeReq = 0;
+        for(int p: piles){
+            timeReq += (p/k) + (p%k ? 1 : 0);
+        }
+        return timeReq <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int res = INT_MAX;
+        int mn = 1, mx = INT_MAX;
+        while(mn <= mx){
+            int mid = mn + (mx-mn)/2;
+            if(willEat(mid, piles, h)){
+                res = min(res, mid);
+                mx = mid - 1;
+            }else
+                mn = mid + 1;
+        }
+        return res;
+    }
+};
